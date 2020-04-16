8aca9c7086966cb25c435e4660c8e157644be526
Kristopher Newman <misterk71@gmail.com>
2020-04-16 09:42:52 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,35 +13,35 @@ variable "vsphere_password" {
 variable "vsphere_server" {
   description	= "Primary vsphere server"
   type    = string
-  default		= ""
+  default		= "cobalt.khaosx.net"
 }
 
 variable "vsphere_datacenter" {
   description	= "Primary vsphere datacenter"
   type    = string
-  default		= ""
+  default		= "Main"
 }
 
 variable "vsphere_datastore" {
   description	= "Primary vsphere datastore"
   type    = string
-  default		= ""
+  default		= "Primary"
 }
 
 variable "vsphere_network" {
   description	= "Primary network name"
   type    = string
-  default		= ""
+  default		= "VM Network"
 }
 
 variable "vsphere_network_san" {
   description	= "SAN Network name"
   type    = string
-  default		= ""
+  default		= "SAN"
 }
 
 variable "vsphere_resource_pool" {
   description	= "Primary vsphere resource pool"
   type    = string
-  default		= ""
+  default		= "nitrogen.khaosx.net/Resources"
 }
